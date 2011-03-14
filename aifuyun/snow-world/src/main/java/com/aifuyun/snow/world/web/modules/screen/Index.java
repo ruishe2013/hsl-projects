662e27aa55e98a3bff54bc8860566337a56af584
pisting <pisting@ad7214ce-87e6-cbf1-4556-9dbab205f930>
2011-03-14 14:51:08 +0000

    首页暂时重定向到拼的首页

---
+++ aifuyun/snow-world/src/main/java/com/aifuyun/snow/world/web/modules/screen/Index.java
@@ -0,0 +1,16 @@
+package com.aifuyun.snow.world.web.modules.screen;
+
+import com.aifuyun.snow.world.web.common.base.BaseScreen;
+import com.zjuh.splist.web.RunData;
+import com.zjuh.splist.web.TemplateContext;
+
+public class Index extends BaseScreen {
+
+	@Override
+	public void execute(RunData rundata, TemplateContext templateContext) {
+		// TODO
+		
+		this.sendRedirect("snowModule", "taxiIndex");
+	}
+
+}
