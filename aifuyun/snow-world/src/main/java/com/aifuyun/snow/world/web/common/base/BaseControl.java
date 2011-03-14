c5cb67d195a89eedba1be5f0131f9c2a9e54b8f1
pisting <pisting@ad7214ce-87e6-cbf1-4556-9dbab205f930>
2011-03-14 15:10:23 +0000

    增加临时其他业务页面

--- aifuyun/snow-world/src/main/java/com/aifuyun/snow/world/web/common/base/BaseControl.java
+++ aifuyun/snow-world/src/main/java/com/aifuyun/snow/world/web/common/base/BaseControl.java
@@ -1,16 +1,10 @@
 package com.aifuyun.snow.world.web.common.base;
 
-import com.zjuh.splist.core.SplistContext;
-import com.zjuh.splist.core.module.Module;
 import com.zjuh.splist.web.RunData;
 import com.zjuh.splist.web.TemplateContext;
 
 public abstract class BaseControl extends BaseModule {
 	
-	protected Module getCurrentScreenModule() {
-		return SplistContext.getCurrentScreenModule();
-	}
-
 	public abstract void execute(RunData rundata, TemplateContext templateContext);
 	
 }
