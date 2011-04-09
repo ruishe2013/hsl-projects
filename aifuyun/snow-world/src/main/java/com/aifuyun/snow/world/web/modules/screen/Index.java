fe78a066b2b0a0c07422cadeaf890a5b7473793b
pisting <pisting@ad7214ce-87e6-cbf1-4556-9dbab205f930>
2011-04-09 11:54:52 +0000

    首页调整

--- aifuyun/snow-world/src/main/java/com/aifuyun/snow/world/web/modules/screen/Index.java
+++ aifuyun/snow-world/src/main/java/com/aifuyun/snow/world/web/modules/screen/Index.java
@@ -1,14 +1,28 @@
 package com.aifuyun.snow.world.web.modules.screen;
 
+import com.aifuyun.snow.world.biz.ao.misc.SnowWorldAO;
 import com.aifuyun.snow.world.web.common.base.BaseScreen;
 import com.zjuh.splist.web.RunData;
 import com.zjuh.splist.web.TemplateContext;
+import com.zjuh.sweet.result.Result;
 
 public class Index extends BaseScreen {
 
+	private SnowWorldAO snowWorldAO;
+	
 	@Override
 	public void execute(RunData rundata, TemplateContext templateContext) {
-		// TODO
+		Result result = snowWorldAO.handleForTaxiIndex();
+		if (result.isSuccess()) {
+			this.result2Context(result, templateContext);
+		} else {
+			this.handleError(result, rundata, templateContext);
+		}
 	}
+
+	public void setSnowWorldAO(SnowWorldAO snowWorldAO) {
+		this.snowWorldAO = snowWorldAO;
+	}
+
 
 }
