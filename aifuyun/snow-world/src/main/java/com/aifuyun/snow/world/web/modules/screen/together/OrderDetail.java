4b938bd5b09632d2276490fce956fcc76cd0e07d
pisting <pisting@ad7214ce-87e6-cbf1-4556-9dbab205f930>
2011-04-09 13:00:31 +0000

    sns分享

--- aifuyun/snow-world/src/main/java/com/aifuyun/snow/world/web/modules/screen/together/OrderDetail.java
+++ aifuyun/snow-world/src/main/java/com/aifuyun/snow/world/web/modules/screen/together/OrderDetail.java
@@ -1,6 +1,7 @@
 package com.aifuyun.snow.world.web.modules.screen.together;
 
 import com.aifuyun.snow.world.biz.ao.together.taxi.OrderAO;
+import com.aifuyun.snow.world.dal.dataobject.together.OrderDO;
 import com.aifuyun.snow.world.web.common.base.BaseScreen;
 import com.zjuh.splist.web.RunData;
 import com.zjuh.splist.web.TemplateContext;
@@ -17,9 +18,16 @@ public class OrderDetail extends BaseScreen {
 		if (result.isSuccess()) {
 			this.result2Context(result, templateContext);
 			
+			// #set($title="从 $!{order.fromCity}$!{order.fromAddr} 到 $!{order.arriveCity}$!{order.arriveAddr} 的拼车")
+			OrderDO order = (OrderDO)result.getModels().get("order");
+			templateContext.put("title", buildTitle(order));
 		} else {
 			this.handleError(result, rundata, templateContext);
 		}
+	}
+	
+	protected String buildTitle(OrderDO order) {
+		return "从 "+ order.getFromCity() + order.getFromAddr() +" 到 "+ order.getArriveCity() + order.getArriveAddr() + " 的拼车";
 	}
 
 	public void setOrderAO(OrderAO orderAO) {
