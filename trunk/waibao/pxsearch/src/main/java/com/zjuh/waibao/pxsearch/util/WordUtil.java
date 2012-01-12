fa89b513f741ae615f9df79bd13b5ff855a0ba46
pisting <pisting@ad7214ce-87e6-cbf1-4556-9dbab205f930>
2012-01-12 14:01:06 +0000

    fixed

--- trunk/waibao/pxsearch/src/main/java/com/zjuh/waibao/pxsearch/util/WordUtil.java
+++ trunk/waibao/pxsearch/src/main/java/com/zjuh/waibao/pxsearch/util/WordUtil.java
@@ -1,11 +1,23 @@
 package com.zjuh.waibao.pxsearch.util;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
+
+import org.textmining.text.extraction.WordExtractor;
 
 public class WordUtil {
 	
-	public static String getWordText(InputStream is) {
-		return null;
+	public static String getWordText(InputStream is)  {
+		WordExtractor  wordExtractor = new WordExtractor();
+		try {
+			return wordExtractor.extractText(is);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println(getWordText(new FileInputStream("d:/EEWEBÐÞ¸Ä.doc")));
 	}
 
 }
